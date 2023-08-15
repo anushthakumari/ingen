6d9c98033ca87cb27d60ab1f62a1ee0a14e6638f
Anushta <ysb0oat7y9t81bdr9et6ld00n73gz7@bots.bitbucket.org>
2023-08-15 21:08:37 +0530

    configs

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,6 @@
 const configs = {
-	API_BASE_URL: "http://localhost/api",
-	BASE_URL: "http://localhost",
+	API_BASE_URL: "http://64.227.149.180/api",
+	BASE_URL: "http://64.227.149.180",
 };
 
 export default configs;
